741a71377922c7a145bba03198fe01f27dabbb0d
noraj <alexandre.zanni@europe.com>
2023-01-03 00:56:36 +0100

    WIP: defang_ip + defang_uri
    TODO:
    - defang_domain & defang_email
    - write tests
    - write doc

---
+++ lib/ctf_party/defang.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'ipaddr'
+require 'uri'
+
+class String
+  def defang_ip
+    if IPAddr.new(self).ipv4?
+      gsub('.', '[.]')
+    elsif IPAddr.new(self).ipv6?
+      gsub(':', '[:]')
+    else
+      self
+    end
+  end
+
+  def defang_uri
+    uri = URI(self)
+    case uri
+    when URI::HTTP, URI::HTTPS, URI::FTP
+      uri.scheme = uri.scheme.gsub(/t/i, 'X')
+    when URI::WS, URI::WSS
+      uri.scheme.insert(1, 'X')
+    when URI::LDAP, URI::LDAPS
+      uri.scheme.insert(2, 'X')
+    when URI::MailTo
+      uri.scheme.insert(4, 'X')
+    end
+    uri.to_s.gsub('.', '[.]')
+  end
+
+  def defang_domain
+    skip
+  end
+
+  def defang_email
+    skip
+  end
+end
